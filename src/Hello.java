a209542f1b49d09ca42c212246a5de8d997d424e
liuhongli <liuhongli@liuhonglis-MacBook-Pro.local>
2019-03-08 16:01:08 +0800

    *What is Variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
     public  static void main(String[] args) {
         System.out.println("Hello World, I am Java!");
+
+        /// What is Variables
+        int myFirstNumber = 5;
+        int mySecondNumber = 5;
+        int myThirdNumber = 5;
+
+        int totalNumber = myFirstNumber + mySecondNumber + myThirdNumber * 3;
+
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber====:" + totalNumber);
     }
 }
